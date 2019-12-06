b8c54af78cea2e3b40b545a1f6ec2b00776acda9
marty109 <denghuikd@qq.com>
2019-12-06 20:14:40 +0800

    添加「BST前中后三种遍历」

--- tree/binary-search-tree.js
+++ tree/binary-search-tree.js
@@ -10,6 +10,7 @@ class BinarySearchTree {
   constructor() {
     this.root = null
   }
+
   insert(key) {
     const newNode = new Node(key)
     const insertNode = (node, newNode) => {
@@ -33,6 +34,40 @@ class BinarySearchTree {
       insertNode(this.root, newNode)
     }
   }
+
+  inorderTraversal(callback) {
+    const inorderTraversalNode = (node, callback) => {
+      if (node !== null) {
+        inorderTraversalNode(node.left, callback)
+        callback(node.key)
+        inorderTraversalNode(node.right, callback)
+      }
+    }
+    inorderTraversalNode(this.root, callback)
+  }
+
+  preorderTraversal(callback) {
+    const preorderTraversalNode = (node, callback) => {
+      if (node !== null) {
+        callback(node.key)
+        preorderTraversalNode(node.left, callback)
+        preorderTraversalNode(node.right, callback)
+      }
+    }
+    preorderTraversalNode(this.root, callback)
+  }
+
+  postorderTraversal(callback) {
+    const postorderTraversalNode = (node, callback) => {
+      if (node !== null) {
+        postorderTraversalNode(node.left, callback)
+        postorderTraversalNode(node.right, callback)
+        callback(node.key)
+      }
+    }
+    postorderTraversalNode(this.root, callback)
+
+  }
 }
 
 const tree = new BinarySearchTree()
@@ -42,3 +77,9 @@ tree.insert(5)
 tree.insert(8)
 tree.insert(12)
 tree.insert(13)
+console.log('inoder traversal :')
+tree.inorderTraversal(value => { console.log(value) })
+console.log('pre traversal :')
+tree.preorderTraversal(value => { console.log(value) })
+console.log('post traversal :')
+tree.postorderTraversal(value => { console.log(value) })
